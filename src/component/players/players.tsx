dbcbc08770c5d57b006351a6e3da18ce2c600e99
Antor11809 <antormainuddin8@gmail.com>
2026-09-17 01:11:40 +0600

    commit-tsrif

--- src/component/players/players.tsx
+++ src/component/players/players.tsx
@@ -1,6 +1,7 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { Iplayer } from "../../types/playerType";
 import AvailablePlayer from "./availableplayer";
+import SelectedPlayers from "./SelectedPlayers";
 interface playerprops{
    playerspromise: Promise<Iplayer[]>
 }
@@ -9,22 +10,33 @@ const Players = ({playerspromise}: playerprops) => {
     console.log(playerspromise);
     const players = use(playerspromise)
     console.log(players, "players")
+    const [buttonType, setbuttonType] = useState("Available")
+
+    const handleUpdateBtnType =  (type: "Available" | "selected") => {
+      setbuttonType(type)
+    };
+
     return (
         <div className = "container mx-auto">
 
 <div className = "flex justify-between gap-4">
-    <h2 className = "font-bold text-2xl">Available players</h2>
+    <h2 className = "font-bold text-2xl">{buttonType === "Available" ? "Available players" : "Selected players"}</h2>
 
 <div>
- <button className="btn btn-info">Availableplayers</button>
- <button className="btn btn-info">Selected</button>
+ <button onClick={ () => handleUpdateBtnType("Available")} className={`btn ${buttonType === "Available" ? "bg-green-400 text-black" : "bg-gray-300"} rounded-r-none`}>
+  Availableplayers
+</button>
+ <button onClick={ () => handleUpdateBtnType("selected")} className={`btn ${buttonType === "selected" ? "bg-green-400 text-black" : "bg-gray-300"} rounded-r-none`}>Selected</button>
 </div>
 
+</div>
+
+{buttonType === "Available" ? (
+  <AvailablePlayer players={players} />
+) : (
+  <SelectedPlayers />
+)}
 
 </div>
-       <AvailablePlayer players={players}/>
-        </div>
-    );
-};
-
+    )};
 export default Players;
